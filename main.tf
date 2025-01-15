38c72a64b1b8cc1bc7be90748dee4c9674699cde
BhanuPrakash <bhanup6@gmail.com>
2025-01-15 15:42:34 -0500

    Initial commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+    region = var.aws_region
+}
+resource "aws_instance" "appserver" {
+    ami = var.ami_id
+    instance_type = var.instance_type
+    tags = {
+      Name="TaskmgmtAppserver"
+    }
+  
+}
+resource "aws_security_group" "app_sg" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound web traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+    default = "us-east-1"
+  
+}
+variable "ami_id" {
+  default = "ami-05576a079321f21f8"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
